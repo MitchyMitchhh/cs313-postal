b9f7c93eeaab5f056ae62314332f01b790bc1d1f
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-03-18 15:12:43 -0600

    stuff

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => { 
-    res.render('form'); 
+    res.render('form.ejs'); 
 });
 
 app.get("/calculate", calculatePostal);
